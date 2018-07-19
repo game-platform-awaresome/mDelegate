e6465bb581300de4f6caf213042cdb54cca2f8a4
Sing.Yi <23323231@qq.com>
2018-07-19 15:32:35 +0800

    Initial Commit

---
+++ M185SDK/M185SDK.h
@@ -0,0 +1,19 @@
+//
+//  M185SDK.h
+//  M185SDK
+//
+//  Created by 燚 on 2018/7/19.
+//  Copyright © 2018年 Sans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for M185SDK.
+FOUNDATION_EXPORT double M185SDKVersionNumber;
+
+//! Project version string for M185SDK.
+FOUNDATION_EXPORT const unsigned char M185SDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <M185SDK/PublicHeader.h>
+
+
